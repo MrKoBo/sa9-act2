d68b8d75c4262716c68e09d12aa2e373b8f1e203
MrKoBo <kdtate4@memphis.edu>
2024-03-16 01:28:33 -0500

    Prob1 testing done

---
+++ spec/prob1_spec.rb
@@ -0,0 +1,25 @@
+# spec/prob1_spec.rb
+require 'prob1'
+RSpec.describe User do
+  let(:user) { User.new("test_user") }
+
+  describe "#log_in" do
+    it "logs the user in" do
+      user.log_in
+      expect(user.logged_in).to be true
+    end
+  end
+
+  describe "#log_out" do
+    it "logs the user out" do
+      user.log_out
+      expect(user.logged_in).to be false
+    end
+  end
+
+  describe "#username" do
+    it "returns the correct username" do
+      expect(user.username).to eq("test_user")
+    end
+  end
+end
